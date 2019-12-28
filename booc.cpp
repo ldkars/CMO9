7e410f6e0c23e970944bb1b9acb9ed06dcef0397
unknown <igra.22play@bk.ru>
2019-12-29 02:59:29 +0300

    push fix booc .cpp .h

---
+++ booc.cpp
@@ -0,0 +1,66 @@
+#include "booc.h"
+#define GetIndex(_index,_size) ( ( (_index%_size) + _size )%_size )
+
+BOOC::BOOC(BufferController &bufferController, DeviceController &deviceController, SourceControlleer &sourceController, int count_generation)
+{
+    this->linkBufferController = &bufferController;
+    this->linkDeviceController = &deviceController;
+    this->linkSourceController = &sourceController;
+    this->count_generation = count_generation;
+}
+
+void BOOC::modeling(){
+    if(linkDeviceController->getStatusInsert(linkSourceController->getGhostRequest()))
+    {
+        if(linkDeviceController->getStatusInsert(linkBufferController->getCopyRequest()))
+        {
+            linkDeviceController->insert(linkBufferController->getRequest());
+        }else
+        {
+            linkDeviceController->insert(linkSourceController->getRequest());
+        }
+    }
+    else
+    {
+        if(!linkBufferController->insert(linkSourceController->getRequest()))
+        {
+            if(linkDeviceController->getStatusInsert(linkBufferController->getCopyRequest()))
+            {
+                linkDeviceController->insert(linkBufferController->getRequest());
+            }
+
+        }else
+        {
+            if(linkDeviceController->getStatusInsert(linkBufferController->getCopyRequest()))
+            {
+                linkDeviceController->insert(linkBufferController->getRequest());
+            }
+        }
+    }
+
+
+    qDebug() << "----------------------------------------------";
+    qDebug() << "         DEVICES";
+    linkDeviceController->TESTPRINTDEVICE();
+    qDebug() << "Количество отказов: " << linkBufferController->error;
+    qDebug() <<"      STATUS BUFFER                 ";
+    linkBufferController->buffPrint();
+    vec_buff.push_back(*linkBufferController);
+    vec_device.push_back(*linkDeviceController);
+    vec_source.push_back(*linkSourceController);
+}
+
+void BOOC::START(){
+
+    for(int j = 0; j < this->count_generation; j++)
+    {
+        modeling();
+    }
+
+    vec_final_req_buff = linkBufferController->getBuffvec(); //окончание моделирования. Фиксируем последние заявки
+
+    percent_failure = linkBufferController->error / count_req;
+}
+
+
+
---
+++ booc.h
@@ -0,0 +1,32 @@
+#ifndef BOOC_H
+#define BOOC_H
+#include "buffercontroller.h"
+#include "devicecontroller.h"
+#include "sourcecontrolleer.h"
+#include <vector>
+#include <QDebug>
+#include "source.h"
+#include "logevent.h"
+
+class BOOC
+{
+public:
+    BOOC(BufferController &bufferController, DeviceController &deviceController,SourceControlleer &sourceController, int count_generation);
+    void START();
+    double percent_failure = 0.0;
+    std::vector<BufferController> vec_buff;
+    std::vector<DeviceController> vec_device;
+    std::vector<SourceControlleer> vec_source;
+    std::vector<Request> vec_req;
+    std::vector<LogEvent> vec_log_event;
+    std::vector<Request> vec_final_req_buff;
+private:
+    BufferController *linkBufferController;
+    DeviceController *linkDeviceController;
+    SourceControlleer *linkSourceController;
+    int count_generation = 0;
+    double count_req = 0;
+    void modeling();
+};
+
+#endif // BOOC_H
