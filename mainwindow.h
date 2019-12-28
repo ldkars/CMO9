1e1f7888738a435d072995820d4fc974f147dbca
unknown <igra.22play@bk.ru>
2019-12-29 00:25:22 +0300

    fix bug crash stepmode mainwondow h cpp

---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "source.h"
+#include "request.h"
+#include "buffercontroller.h"
+#include "booc.h"
+#include "devicecontroller.h"
+#include <QDebug>
+#include "stepmode.h"
+#include "sourcecontrolleer.h"
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_StepModeButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    size_t buffer_size = 0;
+    size_t count_device = 0;
+    size_t count_source = 0;
+    int count_request = 0;
+    double alpha = 0.0;
+    double beta = 0.0;
+    double lambda = 0.0;
+    std::vector<Request> vec_request;
+    std::vector<BufferController> vec_buffer_controller;
+    std::vector<DeviceController> vec_device_controller;
+    std::vector<SourceControlleer> vec_source_controller;
+    bool modeling = false;
+};
+#endif // MAINWINDOW_H
